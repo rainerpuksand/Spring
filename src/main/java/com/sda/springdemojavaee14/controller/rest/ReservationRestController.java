161b3b072c0443f034c341201f65850d9c77deff
puksand <rainerpuksand@hotmail.com>
2022-07-09 11:49:41 +0300

    [change] -.gitignore modified

--- src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/rest/ReservationRestController.java
@@ -1,6 +1,7 @@
 package com.sda.springdemojavaee14.controller.rest;
 
 import com.sda.springdemojavaee14.entity.Reservation;
+import com.sda.springdemojavaee14.service.ReservationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,24 +15,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class ReservationRestController {
 
+    private  final ReservationService reservationService;
+
+    public ReservationRestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+
     @GetMapping("/reservations")
     public List<Reservation> getAllReservations() {
         log.info("getting all reservations");
-        return List.of(
-                new Reservation(1L, 12L, LocalDateTime.now(), LocalDateTime.now().plusHours(2),
-                        "Wołek", "51124", "m@op.pl", 10, ""),
-
-                Reservation.builder()
-                        .id(2L)
-                        .tableNumber(12L)
-                        .startBookingTime(LocalDateTime.now())
-                        .endBookingTime(LocalDateTime.now().plusHours(2))
-                        .surname("Pastuszka")
-                        .phoneNumber("555 555 555")
-                        .email("mariusz@sda.pl")
-                        .numberOfPeople(5)
-                        .address("Marszałkowska 5")
-                        .build()
-        );
+        return reservationService.findAllReservations();
     }
 }
